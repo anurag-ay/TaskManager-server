2050ed2f49e45b8fd577b2c95b06798bb9028ad8
anurag-ay <ay1909069@gmail.com>
2023-12-16 18:34:55 +0530

    update category model

---
+++ models/CategoryModel.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const categorySchema = new mongoose.Schema({
+  type: {
+    type: String,
+    required: true,
+    lowercase: true,
+  },
+  user: {
+    type: mongoose.Schema.ObjectId,
+    ref: "User",
+    default: null,
+  },
+  task: [
+    {
+      type: mongoose.Schema.ObjectId,
+      default: null,
+    },
+  ],
+});
+
+const Category = mongoose.model("Category", categorySchema);
+export default Category;
