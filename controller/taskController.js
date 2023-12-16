d1cfad868bd5e5f12b7c1b9845c95b8799f69700
anurag-ay <ay1909069@gmail.com>
2023-12-16 18:35:22 +0530

    add task controller

---
+++ controller/taskController.js
@@ -0,0 +1,75 @@
+import Task from "../models/TaskModel.js";
+import Category from "../models/CategoryModel.js";
+
+// add Task
+export const addTaskController = async (req, res) => {
+  const { title, userId, categoryId } = req.body;
+
+  let task = new Task({
+    title,
+    user: userId,
+    category: categoryId,
+  });
+
+  const savedTask = await task.save();
+
+  await Category.updateOne(
+    { _id: categoryId, user: userId },
+    {
+      $push: { task: savedTask._id },
+    }
+  );
+
+  res.status(200).send(savedTask);
+};
+
+// get  All Task
+export const getAllTaskController = async (req, res) => {
+  const { userId } = req.body;
+  const tasks = await Task.find({ user: userId });
+
+  res.status(200).send(tasks);
+};
+
+// get Task by id
+export const getTaskByIdController = async (req, res) => {
+  const { taskId } = req.params;
+  const task = await Task.findById(taskId);
+  res.status(200).send(task);
+};
+
+// Update Task
+export const updateTaskController = async (req, res) => {
+  const { taskId, newTitle } = req.body;
+
+  const task = await Task.findOneAndUpdate(
+    { _id: taskId },
+    {
+      $set: { title: newTitle },
+    },
+    { new: true }
+  );
+
+  res.status(200).send(task);
+};
+
+// Delete Task
+export const deleteTaskController = async (req, res) => {
+  const { userId, categoryId, taskId } = req.body;
+
+  let task = await Task.findOneAndDelete(
+    { user: userId, _id: taskId },
+    { new: true }
+  );
+
+  if (!task) return res.send("Task already deleted");
+
+  await Category.updateOne(
+    { _id: categoryId, user: userId },
+    {
+      $pull: { task: taskId },
+    }
+  );
+
+  res.status(200).send(task);
+};
