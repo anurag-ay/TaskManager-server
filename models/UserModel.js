0a9229708499abdcb7e9a1fd44b2aac9523d820d
anurag-ay <ay1909069@gmail.com>
2023-12-17 21:34:15 +0530

    update User model

--- models/UserModel.js
+++ models/UserModel.js
@@ -35,6 +35,9 @@ const userSchema = new mongoose.Schema({
       default: null,
     },
   ],
+  allTaskCategory: {
+    type: mongoose.Schema.ObjectId,
+  },
 });
 const User = mongoose.model("Users", userSchema);
 export default User;
