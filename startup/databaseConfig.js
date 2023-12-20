bb93922635de13f641ad9926ff2c9c836ae0e888
anurag-ay <ay1909069@gmail.com>
2023-12-20 18:19:06 +0530

    update database config

--- startup/databaseConfig.js
+++ startup/databaseConfig.js
@@ -1,7 +1,11 @@
 import mongoose from "mongoose";
 
 export default function databaseConfig() {
-  // Setting up database
+  let DATABASE_URL = process.env.DATABASE_URL_DEV;
+
+  if (process.env.NODE_ENV === "production") {
+    DATABASE_URL = process.env.DATABASE_URL_PROD;
+  }
   mongoose
     .connect(process.env.DATABASE_URL_PROD)
     .then(() => console.log("Connected to the MongoDB..."))
